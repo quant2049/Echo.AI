2ade8622ccd141c2adb2f27edb0a69b992697ac5
Kenan Sanu <kenansanu2007@gmail.com>
2025-12-27 15:29:31 -0500

    Audio-recorder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st  
 
 st.title("🎈 Echo Journal")
-st.write(
-         "Your AI  Journal"
-         )
+st.write("Your AI  Journal")
+audio_value = st.audio_input("Record a voice message")
+if audio_value:
+    st.audio(audio_value)
